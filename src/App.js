7e0c508764e85a52f6e0e4a1627c8b74effc04d9
psittacine <psittacine714@gmail.com>
2018-10-31 02:27:59 -0500

    Add hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { slide as Menu } from 'react-burger-menu'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Menu>
+          <p>Sample Item 1</p>
+          <p>Sample Item 2</p>
+          <p>Sample Item 3</p>
+        </Menu>
       </div>
     );
   }
